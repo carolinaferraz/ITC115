4fccd0998ac61b360d6e67e93e53b6fa86a97929
carolinaferraz <43863678+carolinaferraz@users.noreply.github.com>
2018-10-21 16:59:21 -0700

    updates Seasons.java
    version 2 created output within a method called "season"

--- Seasons.java
+++ Seasons.java
@@ -1,38 +1,40 @@
-//insert a date to find out what's the corresponding season of the year.
+// Seasons-version2
+// insert a date to find out what's the corresponding season of the year
+// using a method called 'season'
 
 import java.util.Scanner;
 
-public class Seasons {
+public class SeasonsV2 {
 
 	public static void main(String[] args) {
-		System.out.println("Insert date to find out "
-				+ "its corresponding season!");
+		// introduction
+		System.out.println("Insert date to find out its corresponding season.");
 		System.out.println();
-		
-		
-		// create an instance of an object of type Scanner
+
+		// collect date
 		Scanner console = new Scanner(System.in);
 		
-		// collect date 
 		System.out.print("Enter month (1-12): ");
 		int mon = console.nextInt();
 		System.out.print("Enter day (1-31): ");
 		int day = console.nextInt();
+		
+		// output
+		System.out.println(season(mon, day));
 		console.close();
-		
-		// seasons parameters
-		
-		if((mon==12 && day>=16)||(mon==3 && day<=15)||(mon==1||mon==2)) {
-			System.out.println("Winter");
-			
-		} else if((mon==3 && day>=16)||(mon==6 && day<=15)||(mon==4||mon==5)) {
-			System.out.println("Spring");
-			
-		} else if((mon==6 && day>=16)||(mon==9 && day<=15)||(mon==7||mon==8)) {
-			System.out.println("Summer");
-			
-		} else if((mon==9 && day>=16)||(mon==12 && day<=15)||(mon==10||mon==11)) {
-			System.out.println("Fall");
-		}		
 	}
+		
+		// season method
+	public static String season(int mon, int day) {
+
+		if ((mon==9 && day>=16)||(mon==12 && day<=15)||(mon==10||mon==11))
+			return "Fall";
+		else {
+			if ((mon==3 && day>=16)||(mon==6 && day<=15)||(mon==4||mon==5))
+				return "Spring";
+			else {
+				if ((mon==6 && day>=16)||(mon==9 && day<=15)||(mon==7||mon==8))
+					return "Summer";
+				else {
+					return "Winter"; }}}}
 }
